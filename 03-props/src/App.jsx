8d86726f2d8dc2fdfcccf296a125991cd4067dbf
amanDev1437 <amanpratap1437@gmail.com>
2024-03-19 23:08:05 +0530

    props

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Card from "./Card";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black rounded-xl p-4 mb-4">Tailwind Test</h1>
+      <Card username = {"Aman"}></Card>
+      <Card username={"Ashish"}></Card>
+    </>
+  );
+}
+
+export default App;
